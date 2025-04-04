00d71db93fd67b51e23c68b918902aff237bd78b
jingedits <arneljohn2006@gmail.com>
2025-04-04 19:36:07 +0800

    Update grocery.py

--- grocery.py
+++ grocery.py
@@ -6,9 +6,10 @@ with open('grocery.csv', 'r') as file:
 
         for row in list:
             grocery_list[row[0]] = [row[1], row[2],row[3], row[4]]
+customer_list = {}
+cart = []
 
 def main():
-    customer_list = {}
     account = ["admin","admin123"]
     while True:
         print("=" * 30)
@@ -74,9 +75,14 @@ def admin_management():
             print("Invalid choices")                                
 def menu():    
     while True:
+        for customer in customer_list:
+            bal = customer_list[customer]
+
         print("=" * 30)
         print("Grocery Store System Management")
         print("=" * 30)
+        print(f"Customer Name       Customer balance:")
+        print(f"{customer:<10}          ₱{bal[0]:<6}")
         print("\n1. View Products")
         print("2. Add Cart")
         print("3. Exit\n")
@@ -88,7 +94,15 @@ def menu():
             display_products(grocery_list)
         
         elif option == "2":
-            add_cart()
+            print(f"Number        Product               Quantity                 Price               Stock")
+            for item in grocery_list:
+                gro = grocery_list[item]
+                print(f"{item}             {gro[0]:<15}       {gro[1]:<10}              {gro[2]:<7}             {gro[3]:<5}")
+
+            print("-" * 30)
+            print("SELECT A PRODUCT TO BUY")
+            num = input("Enter a number to select product: ")
+            add_cart(num)
         
         elif option == "3":
             print("Exiting the program. Thank you!")
@@ -108,8 +122,27 @@ def add_item(number,name,quantity,price,stock):
         writer = csv.writer(file)
         writer.writerow([number, name, quantity, price, stock])
 
-def add_cart():
-    pass
+def add_cart(num):
+    for customer in customer_list:
+            bal = customer_list[customer]
+    if num in grocery_list:
+        gro = grocery_list[num]
+        print(f"Product Name     : {gro[0]}")
+        print(f"Product Quantity : {gro[1]}")
+        print(f"Product Price    : ₱{gro[2]}")
+        print(f"Product Stock    :{gro[3]}")
+        try:
+            quantity = int(input("Enter quantity: "))
+            total_price = float(gro[2]) * quantity
+            cart.append((gro[0], total_price))
+            print(f"Added {quantity} of {gro[0]} to cart. Total price: ₱{total_price:.2f}")
+            if total_price > bal[0]:
+                print("Insufficient balance")
+                print(f"Your balance is: ₱{bal[0]:.2f}")
+            else:
+                bal[0] -= total_price
+        except ValueError:
+            print("Invalid quantity. Please enter a number.")
 
 def remove_item():
     pass
